b2eec1402f8f8cc79c9071ea85b8a5fc99049b2a
zywe03 <zywe03@gmail.com>
2026-09-01 01:12:04 +0800

    v1.2.8

--- port-traffic-dog.sh
+++ port-traffic-dog.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-readonly SCRIPT_VERSION="1.2.7"
+readonly SCRIPT_VERSION="1.2.8"
 readonly SCRIPT_NAME="端口流量狗"
 readonly SCRIPT_PATH="$(realpath "$0")"
 readonly CONFIG_DIR="/etc/port-traffic-dog"
